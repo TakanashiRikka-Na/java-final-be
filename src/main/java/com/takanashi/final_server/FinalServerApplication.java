a729605eb7c87bf2c51088323a4da89a670ad382
TakanashiRikka-Na <2576496262@qq.com>
2023-06-17 23:59:14 +0800

    build: 更新依赖与日志

--- src/main/java/com/takanashi/final_server/FinalServerApplication.java
+++ src/main/java/com/takanashi/final_server/FinalServerApplication.java
@@ -1,9 +1,12 @@
-package com.example.final_server;
+package com.takanashi.final_server;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@MapperScan("com.takanashi.final_server.mapper")
 public class FinalServerApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/takanashi/final_server/FinalServerApplicationTests.java
+++ src/test/java/com/takanashi/final_server/FinalServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.final_server;
+package com.takanashi.final_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
