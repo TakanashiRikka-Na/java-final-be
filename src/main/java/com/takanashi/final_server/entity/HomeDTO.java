496584b1941f25133fb5e68e69dba594a7b6a245
TakanashiRikka-Na <2576496262@qq.com>
2023-06-17 23:58:06 +0800

    feat: 定义 Home Mapper

---
+++ src/main/java/com/takanashi/final_server/entity/HomeDTO.java
@@ -0,0 +1,19 @@
+package com.takanashi.final_server.entity;
+
+public class HomeDTO {
+    private long id;
+    private String HomeID;
+    private String leader;
+
+    public String getHomeID() {
+        return HomeID;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getLeader() {
+        return leader;
+    }
+}
---
+++ src/main/java/com/takanashi/final_server/mapper/HomeMapper.java
@@ -0,0 +1,9 @@
+package com.takanashi.final_server.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.takanashi.final_server.entity.Home;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface HomeMapper extends BaseMapper<Home> {
+}
