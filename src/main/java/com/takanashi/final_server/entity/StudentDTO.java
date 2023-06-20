58e7856802218aaa5fcbfe8353f4d762dac2c848
TakanashiRikka-Na <2576496262@qq.com>
2023-06-20 15:04:21 +0800

    chore: 移除冗余的 @Getter 注解

--- src/main/java/com/takanashi/final_server/entity/StudentDTO.java
+++ src/main/java/com/takanashi/final_server/entity/StudentDTO.java
@@ -6,22 +6,10 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Setter
 public class StudentDTO {
     private String Name;
     private String UserID;
     private   String HomeID;
 
-     public String getUserID() {
-          return UserID;
-     }
 
-     public String getHomeID() {
-          return HomeID;
-     }
-
-
-     public String getName() {
-          return Name;
-     }
 }
