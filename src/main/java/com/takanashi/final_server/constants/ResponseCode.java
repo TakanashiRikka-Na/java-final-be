bf163619f5bc28bd8fe8548fce7b4e191650a6d4
TakanashiRikka-Na <2576496262@qq.com>
2023-06-20 09:33:11 +0800

    feat: 新增 几个状态码

--- src/main/java/com/takanashi/final_server/constants/ResponseCode.java
+++ src/main/java/com/takanashi/final_server/constants/ResponseCode.java
@@ -4,15 +4,20 @@ import lombok.Getter;
 
 @Getter
 public enum ResponseCode {
-    OK(0,"success"),
-    SERVER_ERROR(1,"internal error"),
+    OK(0,"success", 200),
+    SERVER_ERROR(1,"internal error", 500),
 
-    USER_DATA_ERROR(2,"user data error");
+    USER_DATA_ERROR(2,"user data error", 400),
+    DATA_NULL(3,"data is null", 404),
+    DATA_EXIST(4,"data existed", 400);
     private final int code;
     private final String message;
 
-    ResponseCode(int code, String message) {
+    private final int status;
+
+    ResponseCode(int code, String message, int status) {
         this.code = code;
         this.message = message;
+        this.status = status;
     }
 }
